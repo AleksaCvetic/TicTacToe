754a694ac4c2c6098ef2c9612105e28df59bf5cf
Aleksa Cvetić <aleksacvetic71@gmail.com>
2021-11-28 21:42:38 +0100

    the beginning
    -Made the HTML & CSS but struggling with the JS.
    -Learned how to use basic Grid Display.
    -Spent about 2 hours making this.

---
+++ main.js
@@ -0,0 +1,123 @@
+var counter = 1;
+var topLeft = document.getElementsById("1").value;
+var topMid = document.getElementsById("2").value;
+var topRight = document.getElementsById("3").value;
+var midLeft = document.getElementsById("4").value;
+var midMid = document.getElementsById("5").value;
+var midRight = document.getElementsById("6").value;
+var botLeft = document.getElementsById("7").value;
+var botMid = document.getElementsById("8").value;
+var botRight = document.getElementsById("9").value;
+
+if (topLeft == "X" && topMid == "X" && topRight == "X") {
+  document.getElementById("results").innerHTML = "Player X wins!!!";
+  window.alert("Player X wins!!!");
+}
+
+function myfnc1() {
+  if (counter == 1) {
+    document.getElementById("1").innerHTML = "X";
+    document.getElementById("div1").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("1").innerHTML = "O";
+    document.getElementById("div1").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc2() {
+  if (counter == 1) {
+    document.getElementById("2").innerHTML = "X";
+    document.getElementById("div2").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("2").innerHTML = "O";
+    document.getElementById("div2").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc3() {
+  if (counter == 1) {
+    document.getElementById("3").innerHTML = "X";
+    document.getElementById("div3").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("3").innerHTML = "O";
+    document.getElementById("div3").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc4() {
+  if (counter == 1) {
+    document.getElementById("4").innerHTML = "X";
+    document.getElementById("div4").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("4").innerHTML = "O";
+    document.getElementById("div4").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc5() {
+  if (counter == 1) {
+    document.getElementById("5").innerHTML = "X";
+    document.getElementById("div5").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("5").innerHTML = "O";
+    document.getElementById("div5").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc6() {
+  if (counter == 1) {
+    document.getElementById("6").innerHTML = "X";
+    document.getElementById("div6").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("6").innerHTML = "O";
+    document.getElementById("div6").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc7() {
+  if (counter == 1) {
+    document.getElementById("7").innerHTML = "X";
+    document.getElementById("div7").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("7").innerHTML = "O";
+    document.getElementById("div7").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc8() {
+  if (counter == 1) {
+    document.getElementById("8").innerHTML = "X";
+    document.getElementById("div8").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("8").innerHTML = "O";
+    document.getElementById("div8").onclick = null;
+    counter = 1;
+  }
+}
+
+function myfnc9() {
+  if (counter == 1) {
+    document.getElementById("9").innerHTML = "X";
+    document.getElementById("div9").onclick = null;
+    counter = 2;
+  } else {
+    document.getElementById("9").innerHTML = "O";
+    document.getElementById("div9").onclick = null;
+    counter = 1;
+  }
+}
